7f26f33cd14fcff78e420085173a39f4311e6843
Ehsan Abadi <[e.abadi@noviuniversity.com]>
2023-08-22 02:29:16 +0200

    Some errors resolved, hoping for no netlify hosting build errors

--- JavaScriptOefening.js
+++ JavaScriptOefening.js
@@ -1,12 +1,12 @@
-let target = {};
-let source = { [Symbol("foo")]: "bar", naam: "Ehsan Alborzi" };
+// let target = {};
+// let source = { [Symbol("foo")]: "bar", naam: "Ehsan Alborzi" };
 
-Object.assign(target, source);
+// Object.assign(target, source);
 
-Object.keys(target).forEach((element) => {
-  console.log(element);
-});
+// Object.keys(target).forEach((element) => {
+//   console.log(element);
+// });
 
-for (const [key, value] of Object.entries(source)) {
-  console.log(`Key: ${key}, Value: ${value}`);
-}
+// for (const [key, value] of Object.entries(source)) {
+//   console.log(`Key: ${key}, Value: ${value}`);
+// }
